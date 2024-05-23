be2964fdb5a52da053dc06ce23dafaa8d08b032e
oussama-el1 <elhadraoui284@gmail.com>
2024-05-23 17:59:07 +0100

    1-simple_pagination.py

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -35,6 +35,10 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """
+            get_page function take a page and page_size and return
+            a list of rows of names data
+        """
         assert isinstance(page, int) and page > 0
         assert isinstance(page_size, int) and page_size > 0
 
