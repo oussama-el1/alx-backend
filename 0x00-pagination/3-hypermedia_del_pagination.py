d70437a9a77a2f2b28f341a3bc9017e91d606020
oussama-el1 <elhadraoui284@gmail.com>
2024-05-23 19:13:54 +0100

    3-hypermedia_del_pagination.py

---
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+"""
+Deletion-resilient hypermedia pagination
+"""
+
+import csv
+from typing import List, Dict
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+        self.__indexed_dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def indexed_dataset(self) -> Dict[int, List]:
+        """Dataset indexed by sorting position, starting at 0
+        """
+        if self.__indexed_dataset is None:
+            dataset = self.dataset()
+            truncated_dataset = dataset[:1000]
+            self.__indexed_dataset = {
+                i: dataset[i] for i in range(len(dataset))
+            }
+        return self.__indexed_dataset
+
+    def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
+        """ get_hyper_index
+        """
+
+        indexed_data = self.__indexed_dataset
+        assert isinstance(index, int) and 0 < index < len(indexed_data)
+
+        hyper = {'index': index, 'next_index': index + page_size,
+                 'page_size': page_size}
+
+        data = []
+        size = 0
+        temp = index
+        while size < page_size:
+            if temp in indexed_data.keys():
+                data.append(indexed_data[temp])
+                size += 1
+            temp += 1
+
+        hyper['data'] = data
+        return hyper
