46dce9833e52bb1edab16d09d113419a3da5704b
oussama-el1 <elhadraoui284@gmail.com>
2024-05-23 17:16:34 +0100

    .\0-simple_helper_function.py

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+0. Simple helper function
+"""
+
+
+def index_range(page: int, page_size: int) -> tuple:
+    """ index_range """
+    start_indx = page - 1
+    end_endex = start_indx * page_size
+
+    return start_indx, end_endex
