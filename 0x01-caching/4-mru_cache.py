89efa371e777dcbf7c948ad672468044ca24d6c8
oussama-el1 <elhadraoui284@gmail.com>
2024-05-28 08:18:13 +0100

    mru

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -23,9 +23,9 @@ class MRUCache(BaseCaching):
             if key in self.history:
                 self.history.remove(key)
             elif len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-                lru_key = self.history.pop(-1)
-                del self.cache_data[lru_key]
-                print(f'DISCARD: {lru_key}')
+                mru_key = self.history.pop(-1)
+                del self.cache_data[mru_key]
+                print(f'DISCARD: {mru_key}')
             self.cache_data[key] = item
             self.history.append(key)
 
