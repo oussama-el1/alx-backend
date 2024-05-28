c73034087c5f6f4e45032c5618a824f21ed09a44
oussama-el1 <elhadraoui284@gmail.com>
2024-05-28 08:08:34 +0100

    solve

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -9,37 +9,31 @@ BaseCaching = __import__('base_caching').BaseCaching
 
 class LRUCache(BaseCaching):
     """
-    class LRUCache
+    LRUCache class implements an LRU caching mechanism.
     """
-
     def __init__(self):
-        """
-        constractor
-        """
         super().__init__()
         self.history = []
 
     def put(self, key, item):
         """
-        Put to a cache data a item
+        Add an item in the cache
         """
         if key and item:
-            self.cache_data[key] = item
-            self.history.append(key)
-            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-                lru_key = self.history[0]
+            if key in self.history:
+                self.history.remove(key)
+            elif len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                lru_key = self.history.pop(0)
                 del self.cache_data[lru_key]
                 print(f'DISCARD: {lru_key}')
-                self.history.remove(lru_key)
+            self.cache_data[key] = item
+            self.history.append(key)
 
     def get(self, key):
         """
-        get item from cache Storage
+        Get an item by key
         """
-        if key in self.history:
+        if key in self.cache_data:
             self.history.remove(key)
-        if len(self.history) >= BaseCaching.MAX_ITEMS:
-            self.history[-1] = key
-        else:
             self.history.append(key)
         return self.cache_data.get(key, None)
