ddb662f8a2abfb0d183f6207113d63f1e351677b
oussama-el1 <elhadraoui284@gmail.com>
2024-06-16 16:50:43 +0100

    task 2

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -31,7 +31,7 @@ def get_locale():
 @app.route('/')
 def index() -> str:
     """ Route app """
-    return render_template('0-index.html')
+    return render_template('2-index.html')
 
 
 if __name__ == "__main__":
--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -21,10 +21,17 @@ app.config['BABEL_DEFAULT_TIMEZONE'] = Config.BABEL_DEFAULT_TIMEZONE
 babel = Babel(app)
 
 
+@babel.localeselector
+def get_locale():
+    """ select the langueage based on the header http """
+
+    return request.accept_languages.best_match(Config.LANGUAGES)
+
+
 @app.route('/')
 def index() -> str:
     """ Route app """
-    return render_template('1-index.html')
+    return render_template('3-index.html')
 
 
 if __name__ == "__main__":
